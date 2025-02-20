803c8743fe174d5047838d0c6abafd0fb75bc1f9
Umayr_A1i <umayrali123@gmail.com>
2025-02-20 10:40:35 +0000

    Testing commits

--- monitoring_requests.py
+++ monitoring_requests.py
@@ -73,3 +73,6 @@ for website in websites_list:
             print(found_message)
             # Send the found message to Discord
             send_discord_notification(found_message)
+
+            #test to see if the git hub commit is working 
+    
